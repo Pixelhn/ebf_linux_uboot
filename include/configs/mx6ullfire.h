995236afab0508e5de6de7ab03ac7b899704b0c0
rock5b_lhn <pixelhn@qq.com>
2023-08-22 22:47:50 +0800

    delate uboot bootargs

--- include/configs/mx6ullfire.h
+++ include/configs/mx6ullfire.h
@@ -221,7 +221,6 @@
 	"setenv mmcdev 0;" \
 	"setenv bootpart 0:2;" \
 	"setenv rootfpart 1:3;" \
-	"setenv bootargs console=ttymxc0 root=/dev/mmcblk1p3 rw rootfstype=ext4 rootwait;" \
 	"load ${devtype} ${bootpart} 0x80800000 /boot/zImage;"\
 	"load ${devtype} ${bootpart} 0x83000000 /boot/imx6ull-mmc-npi.dtb;" \
 	"bootz 0x80800000 - 0x83000000;\0"
